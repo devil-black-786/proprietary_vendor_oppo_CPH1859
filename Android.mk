60af99b26e001068248bb2a6bb28e685c0fcc720
Ayush <ayush@disroot.org>
2020-07-11 19:18:58 +0530

    CPH1859: Add ImsService
    Signed-off-by: Ayush <ayush@disroot.org>

--- Android.mk
+++ Android.mk
@@ -16,4 +16,17 @@
 
 LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),CPH1859)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := ImsService
+LOCAL_MODULE_OWNER := oppo
+LOCAL_SRC_FILES := proprietary/priv-app/ImsService/ImsService.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_PRIVILEGED_MODULE := true
+include $(BUILD_PREBUILT)
+
 endif
